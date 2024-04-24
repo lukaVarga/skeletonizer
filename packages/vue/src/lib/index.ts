71898569b059133f6133a89fb965bb05eabf7039
lukaVarga <varga.luka93@gmail.com>
2024-04-25 00:46:17 +0200

    fix(vue): installation

--- packages/vue/src/lib/index.ts
+++ packages/vue/src/lib/index.ts
@@ -1,15 +1,3 @@
-import type { App, DirectiveBinding } from 'vue';
 import SkeletonizerSkeleton from './components/SkeletonizerSkeleton.vue';
-import { type ISkeletonizerColorSchema, SkeletonDirective } from '@skeletonizer/utils';
-
-export default {
-  install: (app: App): void => {
-    app.component('SkeletonizerSkeleton', SkeletonizerSkeleton);
-
-    app.directive('skeletonize', (el: HTMLElement, binding: DirectiveBinding<ISkeletonizerColorSchema>) => {
-      SkeletonDirective.skeletonizeProjectedTemplate(el, binding.value);
-    });
-  },
-};
 
 export { SkeletonizerSkeleton };
--- packages/vue/src/main.ts
+++ packages/vue/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import skeletonizer from './lib';
+import skeletonizer from './lib/main';
 import App from './App.vue';
 import './assets/main.css';
 
